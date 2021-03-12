42e14a7e5d99c8fba712857cb5c5625f2a2a5918
Manas Aggarwal <manas.agrawal990@gmail.com>
2021-03-12 12:42:31 +0530

    Add info.plist

--- Sources/Ripple/RippleButton.swift
+++ Sources/Ripple/RippleButton.swift
@@ -9,30 +9,42 @@ import SwiftUI
 
 public struct RippleButton: View {
     
+    var width: CGFloat
+    var height: CGFloat
+    var color: Color
+    var text: String
     @State var isTapped: Bool = false
     
-    public init() { }
+    public init(width: CGFloat,
+                height: CGFloat,
+                color: Color,
+                text: String) {
+        self.width = width
+        self.height = height
+        self.color = color
+        self.text = text
+    }
     
     public var body: some View {
         Button(action: { isTapped.toggle() }, label: {
-            Text("Next")
+            Text(text)
                 .font(.system(size: 18, weight: .regular))
-                .frame(width: 75, height: 15)
+                .frame(width: width, height: height)
                 .padding(.all)
                 .foregroundColor(.white)
                 .background(
                     ZStack {
-                        Color.primeblue
+                        color
                         // background color
                         RoundedRectangle(cornerRadius: 30, style: .circular)
-                            .foregroundColor(Color.primeblue)
+                            .foregroundColor(color)
                         .blur(radius: 1)
                         .offset(x: -1, y: -1)
                         
                         // inner gradient shadow
                         RoundedRectangle(cornerRadius: 30, style: .circular)
                         .fill(
-                            LinearGradient(gradient: Gradient(colors: [Color.background.opacity(0.4), Color.primeblue.opacity(0.1)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            LinearGradient(gradient: Gradient(colors: [Color.background.opacity(0.4), color.opacity(0.1)]), startPoint: .topLeading, endPoint: .bottomTrailing)
                         )
                         .padding(3.5)
                         .blur(radius: 1)
@@ -45,47 +57,12 @@ public struct RippleButton: View {
                 // light shadow
                 .shadow(color: Color.white.opacity(0.15), radius: 10, x: -7, y: -5)
         })
-//        VStack(spacing: 32) {
-//            Button(action: { isTapped.toggle() }, label: {
-//                Text("Next")
-//                    .font(.system(size: 18, weight: .regular))
-//                    .frame(width: 75, height: 15)
-//                    .padding(.all)
-//                    .foregroundColor(.white)
-//                    .background(
-//                        ZStack {
-//                            Color.primeblue
-//                            // background color
-//                            RoundedRectangle(cornerRadius: 30, style: .circular)
-//                                .foregroundColor(Color.primeblue)
-//                            .blur(radius: 1)
-//                            .offset(x: -1, y: -1)
-//
-//                            // inner gradient shadow
-//                            RoundedRectangle(cornerRadius: 30, style: .circular)
-//                            .fill(
-//                                LinearGradient(gradient: Gradient(colors: [Color.background.opacity(0.4), Color.primeblue.opacity(0.1)]), startPoint: .topLeading, endPoint: .bottomTrailing)
-//                            )
-//                            .padding(3.5)
-//                            .blur(radius: 1)
-//
-//                        }
-//                    )
-//                    .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-//                    // dark shadow
-//                    .shadow(color: Color.black.opacity(0.8), radius: 10, x: 7, y: 5)
-//                    // light shadow
-//                    .shadow(color: Color.white.opacity(0.15), radius: 10, x: -7, y: -5)
-//            })
-//        }
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(Color.background).ignoresSafeArea(.all)
     }
 }
 
 struct RippleButton_Previews: PreviewProvider {
     static var previews: some View {
-        RippleButton()
+        RippleButton(width: 75, height: 15, color: Color.primeblue, text: "Next")
     }
 }
 
